85d6a5032782ac44eb94c0e324c48d06db8c8bd2
chyngyzturap <chyngyzturapov@gmail.com>
2025-09-25 19:21:52 +0600

    test

--- app/src/main/java/com/cht/mybank12m/ui/MainActivity.kt
+++ app/src/main/java/com/cht/mybank12m/ui/MainActivity.kt
@@ -18,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
     private lateinit var adapter: AccountAdapter
